fdaa5d8f61ae7f0d2c1c3e5b11458beeb79d0b90
Devin Bidwell <dbidwell94@gmail.com>
2020-12-09 00:48:28 -0800

    renamed the it() on one of the byteUtils tests

--- src/utils/byteUtils.unit.test.ts
+++ src/utils/byteUtils.unit.test.ts
@@ -9,7 +9,7 @@ import {
 } from "../shared/testHelper";
 
 describe("src/utils/byteUtils.ts", () => {
-  it("Can convert a base64 string into a Uint64 array", () => {
+  it("Can convert a base64 string into a Uint8 array", () => {
     const base64String = btoa("this is a test string");
     const arr = base64ToUint8(base64String);
     expect(arr).toBeInstanceOf(Uint8Array);

bc72cfb5cdfbcd9e20cc643dc5a80d782dfd80ef
Devin Bidwell <dbidwell94@gmail.com>
2020-12-08 23:59:09 -0800

    byte-utils tests

--- src/utils/byteUtils.ts
+++ src/utils/byteUtils.ts
@@ -1,6 +1,6 @@
 import { MutableRefObject, RefObject } from "react";
 import { iRequestObject, iBufferedRange, iResponseObject } from "./types";
-import axios from "axios";
+import axios from 'axios';
 
 export function base64ToUint8(input: string): Uint8Array {
   return Uint8Array.from(atob(input), (c) => c.charCodeAt(0));
--- src/utils/byteUtils.unit.test.ts
+++ src/utils/byteUtils.unit.test.ts
@@ -1,5 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {base64ToUint8} from './byteUtils';
+
 describe("src/utils/byteUtils.ts", () => {
     it("Can convert a base64 string into a Uint64 array", () => {
+
         
+        const base64String = btoa("this is a test string");
+        const arr = base64ToUint8(base64String);
+        expect(arr).toBeInstanceOf(Uint8Array);
     })
 })

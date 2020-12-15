0bfc5f536f59fa14e48ecc59452dcd5369d68495
Devin Bidwell <dbidwell94@gmail.com>
2020-12-14 23:50:44 -0800

    updated webpack build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,85 @@
 const path = require("path");
 const htmlPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    app: path.join(__dirname, "src", "index.tsx"),
-  },
-  target: "web",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-        ],
-      },
-      {
-        test: /\.less$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-          },
-          {
-            loader: "less-loader",
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
-      },
-    ],
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new htmlPlugin({ filename: "index.html", template: "./src/index.html" }),
-  ],
-  performance: {
-    hints: false,
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "async",
-      minSize: 10000,
-      maxSize: 80000,
-      maxAsyncRequests: 30,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          reuseExistingChunk: true,
+module.exports = (env, argv) => {
+  return {
+    mode: argv.mode || "development",
+    entry: {
+      app: path.join(__dirname, "src", "index.tsx"),
+    },
+    target: "web",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+            },
+          ],
         },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
+        {
+          test: /\.less$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "style-loader",
+            },
+            {
+              loader: "css-loader",
+            },
+            {
+              loader: "less-loader",
+            },
+          ],
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+            },
+          ],
+        },
+      ],
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    plugins: [
+      new htmlPlugin({ filename: "index.html", template: "./src/index.html" }),
+    ],
+    performance: {
+      hints: false,
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "async",
+        minSize: 10000,
+        maxSize: 80000,
+        maxAsyncRequests: 30,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
         },
       },
     },
-  },
+  };
 };

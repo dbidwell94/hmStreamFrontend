500566c4a041cd181fcc71e7371c32367d127253
Devin Bidwell <devin.bidwell@accretivetg.com>
2020-12-15 17:11:41 -0800

    development only has redux logger

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./components";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, Reducer } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import baseReducer from "./reducer/baseReducer";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import "./less/index.less";
 
-const store = createStore(
-  baseReducer,
-  applyMiddleware(thunk, logger)
+const isDevelopment = !process.env.NODE_ENV?.toLowerCase().includes(
+  "production"
 );
+console.log(process.env);
+
+const store = isDevelopment
+  ? createStore(baseReducer, applyMiddleware(thunk, logger))
+  : createStore(baseReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
